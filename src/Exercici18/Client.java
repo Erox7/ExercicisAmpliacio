76ee04b7f012dd581a162f0a7647186bb052b452
Erox7 <trugove@hotmail.com>
2018-05-15 00:08:09 +0200

    Link added

--- src/Exercici18/Client.java
+++ src/Exercici18/Client.java
@@ -1,6 +1,6 @@
 package Exercici18;
 
-
+// https://sophia.javeriana.edu.co/~lcdiaz/ADOO2006-3/GoF_FactoryMethod.pdf
 public class Client {
     public static void main(String[] args) {
         Figure f = new Line(1,2);
